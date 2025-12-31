a8e6baef765917af545046fc5669c06a870740f4
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-31 15:44:10 +0100

    create product model

--- main/models.py
+++ main/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Product(models.Model) :
+    name  = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    image = models.ImageField(upload_to='products')
+    description = models.TextField(null=True)
+
 class Auction(models.Model) :
     name = models.CharField(max_length=100)
     start_price = models.DecimalField(max_digits=6,decimal_places=2)
