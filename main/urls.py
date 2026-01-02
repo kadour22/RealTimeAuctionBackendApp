4611dafd4d032fff733f98b03c69b7e65b5131d8
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-02 17:48:16 +0100

    update

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        "create-auction/", views.create_auction_view.as_view()
-    ),
+    path("create-auction/", views.create_auction_view.as_view()),
     path("place-bid/<int:auction_id>/", views.palce_bid.as_view()),
 ]
