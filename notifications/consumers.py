6945f554fc80a756348b93db88e63b0649e8022c
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-06 21:12:12 +0100

    create mark as read notification

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,5 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Notification
 
 class notifications_consumer(AsyncWebsocketConsumer) :
     
@@ -23,4 +25,28 @@ class notifications_consumer(AsyncWebsocketConsumer) :
             "message"    : event["message"],
             "is_read"    : event["is_read"],
             "created_at" : event["created_at"],
+        }))
+
+class mark_as_read_consumer(AsyncWebsocketConsumer) :
+   
+    async def connect(self):
+        self.notification_id = self.scope['url_route']['kwargs']['notification_id']
+        await self.channel_layer.group_add(self.notification_id, self.channel_name)
+        await self.accept()
+    
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.notification_id, self.channel_name)
+    
+    async def receive(self, text_data ):
+        if text_data["type"] == "mark_as_read" :
+            pass
+    
+    @database_sync_to_async
+    def mark_notification_as_read(self, notification_id):
+        Notification.objects.filter(id=notification_id).update(is_read=True)
+    
+    async def notification_read(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "notification_read",
+            "notification_id": event["notification_id"]
         }))
