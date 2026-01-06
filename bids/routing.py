fff601f89a457714695fe55e66ba61cb13485468
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-06 20:55:20 +0100

    create notifications routing

--- bids/routing.py
+++ bids/routing.py
@@ -1,8 +1,8 @@
 from django.urls import re_path
 from . import consumers
+from notifications.consumers import notifications_consumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/auction/(?P<auction_id>\w+)/$',
-            consumers.auction_consumer.as_asgi()),
-
+    re_path(r'ws/auction/(?P<auction_id>\w+)/$', consumers.auction_consumer.as_asgi()),
+    re_path(r'^ws/notifications/$', notifications_consumer.as_asgi()),
 ]
