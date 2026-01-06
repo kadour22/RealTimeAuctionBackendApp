101bc96b57937ef193db5a402803661ea1ad82c1
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-06 16:52:44 +0100

    add routing

--- bids/models.py
+++ bids/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from auctions.models import Auction
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bidding(models.Model) :
+    
+    user       = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids')
+    auction    = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name='auctions')
+    amount     = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} => {self.auction.name}"
+    
---
+++ bids/routing.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/auction/(?P<auction_id>\w+)/$',
+            consumers.auction_consumer.as_asgi()),
+
+]
