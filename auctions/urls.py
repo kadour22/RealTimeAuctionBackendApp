cacc22fce8dd96e99d74aacfaeae701d9339addf
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-06 16:38:21 +0100

    configuer auctions urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('list-create-auction/', views.list_create_auction.as_view()),
+    path('auction/<int:id>/', views.retrieve_update_delete_auction.as_view())
+]
