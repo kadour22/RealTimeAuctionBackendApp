9f3a03cdb66fb2e3e8e04b00f0e18882088c2cff
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-06 16:36:50 +0100

    create && List  auction view

--- auctions/views.py
+++ auctions/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import auction_serializer
+from .models import Auction
 
-# Create your views here.
+class list_create_auction(generics.ListCreateAPIView) :
+    serializer_class = auction_serializer
+    queryset = Auction.objects.select_related('created_by').all()
+
+    def perform_create(self, serializer):
+        return serializer.save(created_by=self.request.user)
+    
