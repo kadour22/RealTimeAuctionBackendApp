0a957845b3a560616156a66335f8a2fde3143d20
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-06 16:37:30 +0100

    retrieve & Update & Destroy  auction view

--- auctions/views.py
+++ auctions/views.py
@@ -9,3 +9,9 @@ class list_create_auction(generics.ListCreateAPIView) :
     def perform_create(self, serializer):
         return serializer.save(created_by=self.request.user)
     
+
+class retrieve_update_delete_auction(generics.RetrieveUpdateDestroyAPIView) :
+    serializer_class = auction_serializer
+    queryset = Auction.objects.select_related('created_by').all()
+    lookup_field = 'id'
+    
