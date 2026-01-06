e6513c2af6a134a4120cd84e83009ff40fc0b696
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-06 16:32:31 +0100

    create && migrate auction model

--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Auction(models.Model):
+    
+    AUCTION_STATUS = (
+        ('open','open'),
+        ('waiting','waiting'),
+        ('closed','closed'),
+        ('ended','ended'),
+    )
 
-# Create your models here.
+    name           = models.CharField(max_length=100)
+    description    = models.TextField()
+    is_active      = models.BooleanField(default=True)
+    auction_status = models.CharField(max_length=30,choices=AUCTION_STATUS,default='waiting')
+
+    start_price   = models.DecimalField(max_digits=6,decimal_places=2)
+    current_price = models.DecimalField(max_digits=6,decimal_places=2, default=0)
+
+    start_date = models.DateTimeField()
+    end_date   = models.DateTimeField()
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='auctions')
+
+    def __str__(self) :
+        return self.name
+
+    def check_auction_status(self) :
+        if self.auction_status == "wating" or self.auction_status == "closed" :
+            raise ValueError("auction closed or not open yet...")
