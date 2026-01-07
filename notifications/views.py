a843c3f51483730cf7467278018d67d12e298631
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-07 22:20:34 +0100

    create notification detail view

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,8 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,4 +15,18 @@ class notifications_list(APIView) :
         notifications = Notification.objects.all()
         serializer    = notification_serializer(notifications, many=True)
         return Response(serializer.data , status=status.HTTP_200_OK)
-    
+
+class notification_detail_view(APIView) :
+    def get(self, request,notification_id) :
+        notification = get_object_or_404(Notification,id=notification_id)
+        channel_layer = get_channel_layer()
+        serializer = notification_serializer(notification,many=False)
+        async_to_sync(channel_layer.group_send)(
+            "notification_read",
+            {
+                "type":"notification_read",
+                "notification_id":notification.id
+            }
+        )
+        return Response(serializer.data,status=200)
+
