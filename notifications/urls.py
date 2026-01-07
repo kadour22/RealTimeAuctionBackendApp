b24e0a2c45f41400d1479d7ef2894e6ec12a56dc
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-01-07 22:28:21 +0100

    create notification detail url

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("notification_list/" , views.notifications_list.as_view())
+    path("notification_list/" , views.notifications_list.as_view()),
+    path("notification/<int:notification_id>/" , views.notification_detail_view.as_view()),
 ]
